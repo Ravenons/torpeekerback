Ravenons/torpeekerback

commit d872c54ffd59a5c6914a9c1189cf1df6d96a3631

Enabled Django logging to console

torpeekerback/settings.py
@@ -115,6 +115,21 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO'),
+        },
+    },
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/

commit 3eeff990efa49dfe75265862ef7b0945bdbe3fcd

Fix syntax error introduced in d872c54ffd59a5c6914a9c1189cf1df6d96a3631

torpeekerback/settings.py
@@ -126,7 +126,7 @@ LOGGING = {
     'loggers': {
         'django': {
             'handlers': ['console'],
-            'level': 'INFO'),
+            'level': 'INFO',
         },
     },
 }