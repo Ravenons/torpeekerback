49fe4cf3dffad569db3ee250dd75c85d366c07e6
Carlos Ledesma <ravenons@gmail.com>
2018-02-28 22:13:35 +0100

    Deleted migration command from gae_deploy.sh and put into deploy auxiliar script (so it works too in local Docker)

--- deploy_resources/deploy_aux.sh
+++ deploy_resources/deploy_aux.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Executing auxiliar deployment script"
 
-# Run auxiliar Python script in Django context
-
 cd $DEPLOY_WORKDIR
+python manage.py migrate
+# Run auxiliar Python script in Django context
 python manage.py shell < $DEPLOY_RESOURCES_DIR/db_data_setup.py
--- gae_deploy.sh
+++ gae_deploy.sh
@@ -13,10 +13,4 @@ fi
 python manage.py collectstatic --no-input
 gsutil -m rsync -r ./static gs://torpeeker/static
 
-# Migrate, but using production DB through cloud_sql_proxy (lol)
-mv torpeekerback/local_settings.py torpeekerback/local_settings.py.bak
-cp deploy_resources/local_settings.py torpeekerback/local_settings.py
-python manage.py migrate
-mv torpeekerback/local_settings.py.bak torpeekerback/local_settings.py
-
 gcloud app deploy --version dev
