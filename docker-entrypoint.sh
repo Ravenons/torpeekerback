a05f4490a6ae995b08952a31d11436751aef4f3f
Carlos Ledesma <ravenons@gmail.com>
2018-03-04 19:05:48 +0100

    Fixed Tor not starting... Shouldn't be here in this container, just testing...

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 echo "Executing Docker entrypoint wrapper script"
 
-# Run auxiliar deploy script and execute what's specified in Dockerfile's CMD
+# This should neither be run in this container nor this way, but...
+tor &
 
+# Run auxiliar deploy script and execute what's specified in Dockerfile's CMD
 $DEPLOY_RESOURCES_DIR/deploy_aux.sh
 exec $@
